a2db757c5f574726a7f7a7e6313a055b82f0f8a2
Adrien Giboire <adrien.giboire@gmail.com>
2013-05-25 18:57:37 +0000

    Update ZSH section

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -20,24 +20,54 @@ fi
 echo "Linking $PWD/githelpers $HOME/.githelpers"
 ln -s $PWD/githelpers $HOME/.githelpers
 
-if [ -L $HOME/.zshrc ]; then
-  echo "Removing $HOME/.zshrc"
-  rm $HOME/.zshrc
-elif [ -f $HOME/.zshrc ]; then
-  echo "Backup $HOME/.zshrc in $HOME/.zshrc.orig"
-  mv $HOME/.zshrc $HOME/.zshrc.orig
+if [ -L /etc/zsh/zlogin ]; then
+  echo "Removing /etc/zsh/zlogin"
+  rm /etc/zsh/zlogin
+elif [ -f /etc/zsh/zlogin ]; then
+  echo "Backup /etc/zsh/zlogin in /etc/zsh/zlogin.orig"
+  mv /etc/zsh/zlogin /etc/zsh/zlogin.orig
 fi
-echo "Linking $PWD/zshrc $HOME/.zshrc"
-ln -s $PWD/zshrc $HOME/.zshrc
+echo "Linking $PWD/zlogin /etc/zsh/zlogin"
+ln -s $PWD/zlogin /etc/zsh/zlogin
 
-if [ -L $HOME/.oh-my-zsh/themes/whoshallsucceed.zsh-theme ]; then
-  echo "Removing $HOME/.oh-my-zsh/themes/whoshallsucceed.zsh-theme"
-  rm $HOME/.zshrc
-elif [ -f $HOME/.oh-my-zsh/themes/whoshallsucceed.zsh-theme ]; then
-  echo "Backup $HOME/.oh-my-zsh/themes/whoshallsucceed.zsh-theme in $HOME/.oh-my-zsh/themes/whoshallsuceed.zsh-theme.orig"
-  mv $HOME/.oh-my-zsh/themes/whoshallsucceed.zsh-theme $HOME/.oh-my-zsh/themes/whoshallsuceed.zsh-theme.orig
+if [ -L /etc/zsh/zlogout ]; then
+  echo "Removing /etc/zsh/zlogout"
+  rm /etc/zsh/zlogout
+elif [ -f /etc/zsh/zlogout ]; then
+  echo "Backup /etc/zsh/zlogout in /etc/zsh/zlogout.orig"
+  mv /etc/zsh/zlogout /etc/zsh/zlogout.orig
 fi
-echo "Linking $PWD/.oh-my-zsh/themes/whoshallsucceed.zsh-theme $HOME/.oh-my-zsh/themes/whoshallsucceed.zsh-theme"
-ln -s $PWD/.oh-my-zsh/themes/whoshallsucceed.zsh-theme $HOME/.oh-my-zsh/themes/whoshallsucceed.zsh-theme
+echo "Linking $PWD/zlogout /etc/zsh/zlogout"
+ln -s $PWD/zlogout /etc/zsh/zlogout
+
+if [ -L /etc/zsh/zshenv ]; then
+  echo "Removing /etc/zsh/zshenv"
+  rm /etc/zsh/zshenv
+elif [ -f /etc/zsh/zshenv ]; then
+  echo "Backup /etc/zsh/zshenv in /etc/zsh/zshenv.orig"
+  mv /etc/zsh/zshenv /etc/zsh/zshenv.orig
+fi
+echo "Linking $PWD/zshenv /etc/zsh/zshenv"
+ln -s $PWD/zshenv /etc/zsh/zshenv
+
+if [ -L /etc/zsh/dir_colors ]; then
+  echo "Removing /etc/zsh/dir_colors"
+  rm /etc/zsh/dir_colors
+elif [ -f /etc/zsh/dir_colors ]; then
+  echo "Backup /etc/zsh/dir_colors in /etc/zsh/dir_colors.orig"
+  mv /etc/zsh/dir_colors /etc/zsh/dir_colors.orig
+fi
+echo "Linking $PWD/dir_colors /etc/zsh/dir_colors"
+ln -s $PWD/dir_colors /etc/zsh/dir_colors
+
+if [ -L /etc/zsh/zshrc ]; then
+  echo "Removing /etc/zsh/zshrc"
+  rm /etc/zsh/zshrc
+elif [ -f /etc/zsh/zshrc ]; then
+  echo "Backup /etc/zsh/zshrc in /etc/zsh/zshrc.orig"
+  mv /etc/zsh/zshrc /etc/zsh/zshrc.orig
+fi
+echo "Linking $PWD/zshrc /etc/zsh/zshrc"
+ln -s $PWD/zshrc /etc/zsh/zshrc
 
 echo "Links created."
