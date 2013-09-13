bd35ef28f36e03627d5ffb0e1c613baf32aa0b97
Adrien Giboire <adrien.giboire@gmail.com>
2013-09-13 10:23:23 +0200

    improv: handle case where zsh is not installed

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -1,22 +1,24 @@
 #!/bin/zsh
 
-FILES=("gitconfig" "githelpers" "zlogin" "zlogout" "zshenv" "zshrc")
+FILES=("gitconfig" "githelpers")
 
 for file in $FILES
 do
-	if [[ -L $HOME/.$file ]]; then
-		echo "Removing $HOME/.$file"
-		rm $HOME/.$file
-	elif [[ -f $HOME/.$file ]]; then
-		echo "Backup $HOME/.$file in $HOME/.$file.orig"
-		mv $HOME/.$file $HOME/.$file.orig
-	fi
+    if [[ -L $HOME/.$file ]]; then
+        echo "Removing $HOME/.$file"
+        rm $HOME/.$file
+    elif [[ -f $HOME/.$file ]]; then
+        echo "Backup $HOME/.$file in $HOME/.$file.orig"
+        mv $HOME/.$file $HOME/.$file.orig
+    fi
 
-	print "Linking $PWD/zshrc $HOME/.$file"
-	ln -s $PWD/$file $HOME/.$file
+    print "Linking $PWD/zshrc $HOME/.$file"
+    ln -s $PWD/$file $HOME/.$file
 done
 
 echo "Links created."
 
-. $HOME/.zshrc
-echo "$HOME/.zshrc sourced"
+if [[ -L $HOME/.zshrc ]]; then
+    . $HOME/.zshrc
+    echo "$HOME/.zshrc sourced"
+fi
