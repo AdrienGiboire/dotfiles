8523ea15a02e10ecf284a2d2817ff0937d67f556
Adrien Giboire <adrien+github@sent.com>
2018-09-11 14:38:21 +0200

    fix: migrate script

--- .config/fish/functions/migrate.fish
+++ .config/fish/functions/migrate.fish
@@ -1,15 +1,15 @@
 function migrate
   # if we have a name for the migration AND some `field:type` property
-  if test 2 -le (count argv)
-    drails g migration argv
+  if test 2 -le (count $argv)
+    drails g migration $argv
     drake db:migrate
 
   # if we just have a migration name
-  else if test 1 -le (count argv)
-    drails g migration argv
+  else if test 1 -le (count $argv)
+    drails g migration $argv
 
   # if we have nothing we just migrate the DB
-  else if test 0 -le (count argv)
+  else if test 0 -le (count $argv)
     drake db:migrate
   end
 end
