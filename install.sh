93379559a2cdbc9324497b9c45a0894e5346d5bf
Adrien <AdrienGiboire@users.noreply.github.com>
2018-09-11 18:41:10 +0200

    feature: auto install ruby-install

--- install.sh
+++ install.sh
@@ -17,3 +17,8 @@ wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.
 tar -xzvf chruby-0.3.9.tar.gz
 cd chruby-0.3.9/
 sudo make install
+
+wget -O ruby-install-0.7.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz
+tar -xzvf ruby-install-0.7.0.tar.gz
+cd ruby-install-0.7.0/
+sudo make install
