6cd1411566b20c7a538bc0709991cef47aefe669
Adrien <AdrienGiboire@users.noreply.github.com>
2018-09-11 18:42:55 +0200

    improv: download sources in `/tmp`

--- install.sh
+++ install.sh
@@ -13,11 +13,13 @@ end
 config checkout
 config config status.showUntrackedFiles no
 
+cd /tmp
 wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
 tar -xzvf chruby-0.3.9.tar.gz
 cd chruby-0.3.9/
 sudo make install
 
+cd /tmp
 wget -O ruby-install-0.7.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz
 tar -xzvf ruby-install-0.7.0.tar.gz
 cd ruby-install-0.7.0/
