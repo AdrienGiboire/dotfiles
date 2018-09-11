c68d57395bfa8f245f11a54ced61118fc6a1e019
Adrien <AdrienGiboire@users.noreply.github.com>
2018-09-11 18:19:16 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+git clone --bare https://github.com/adriengiboire/dotfiles.git $HOME/.dotfiles
+function config
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv
+end
+mkdir -p .config-backup
+config checkout
+if test 0 -le (count $argv)
+  echo "Checked out config."
+else
+    echo "Backing up pre-existing dot files."
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+end
+config checkout
+config config status.showUntrackedFiles no
