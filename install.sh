bb4239d98ca765b81ba9e58140d19b49db8fae11
Adrien Giboire <adrien+github@sent.com>
2018-09-14 19:26:58 +0200

    improv: complete installation process

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+ruby_version=2.5.1
+chruby_version=0.3.9
+chruby_fish_version=0.8.1
+ruby_install_version=0.7.0
+
 git clone --bare https://github.com/adriengiboire/dotfiles.git $HOME/.dotfiles
 function config
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
@@ -14,21 +19,26 @@ config checkout
 config config status.showUntrackedFiles no
 
 cd /tmp
-wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
-tar -xzvf chruby-0.3.9.tar.gz
-cd chruby-0.3.9/
+wget -O ruby-install-$ruby_install_version.tar.gz https://github.com/postmodern/ruby-install/archive/v$ruby_install_version.tar.gz
+tar -xzvf ruby-install-$ruby_install_version.tar.gz
+cd ruby-install-$ruby_install_version/
 sudo make install
 
-cd /tmp
-wget -O ruby-install-0.7.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz
-tar -xzvf ruby-install-0.7.0.tar.gz
-cd ruby-install-0.7.0/
-sudo make install
+mkdir ~/.rubies
+ruby-install ruby-$ruby_version
 
 cd /tmp
-wget -O chruby-fish-0.8.1.tar.gz https://github.com/JeanMertz/chruby-fish/archive/v0.8.1.tar.gz
-tar -xzvf chruby-fish-0.8.1.tar.gz
-cd chruby-fish-0.8.1/
+wget -O chruby-$chruby_version.tar.gz https://github.com/postmodern/chruby/archive/v$chruby_version.tar.gz
+tar -xzvf chruby-$chruby_version.tar.gz
+cd chruby-$chruby_version/
+sudo make install
+
+echo "ruby-$ruby_version" > ~/.ruby-version
+
+cd /tmp
+wget -O chruby-fish-$chruby_fish_version.tar.gz https://github.com/JeanMertz/chruby-fish/archive/v$chruby_fish_version.tar.gz
+tar -xzvf chruby-fish-$chruby_fish_version.tar.gz
+cd chruby-fish-$chruby_fish_version/
 sudo make install
 
 git clone https://github.com/k-takata/minpac.git \
