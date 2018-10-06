d712cc587bf45ec85130050094a0eeb43a89d81e
Adrien Giboire <adrien+github@sent.com>
2018-10-06 11:38:52 +0200

    feature: add shortcut functions to fix watchman config

---
+++ .config/fish/functions/fix_watchman.fish
@@ -0,0 +1,7 @@
+function fix_watchman
+  echo 256 | sudo tee -a /proc/sys/fs/inotify/max_user_instances
+  echo 32768 | sudo tee -a /proc/sys/fs/inotify/max_queued_events
+  echo 65536 | sudo tee -a /proc/sys/fs/inotify/max_user_watches
+  watchman shutdown-server
+end
+
