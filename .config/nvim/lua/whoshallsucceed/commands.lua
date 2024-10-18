5786d4ab3055011315be596134b61e1367e58320
Adrien <github@adrien.sent.com>
2024-10-18 10:06:35 +0200

    fix(vim): macros match with tabs

--- .config/nvim/lua/whoshallsucceed/commands.lua
+++ .config/nvim/lua/whoshallsucceed/commands.lua
@@ -4,8 +4,9 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.fn.setpos("'c", { 0, vim.fn.search('^  computed:', 'n'), 1, 0 })
     vim.fn.setpos("'d", { 0, vim.fn.search('^  data ()', 'n'), 1, 0 })
     vim.fn.setpos("'i", { 0, vim.fn.search('^import', 'n'), 1, 0 })
-    vim.fn.setpos("'m", { 0, vim.fn.search('^methods', 'n'), 1, 0 })
-    vim.fn.setpos("'p", { 0, vim.fn.search('^props', 'n'), 1, 0 })
-    vim.fn.setpos("'w", { 0, vim.fn.search('^watch', 'n'), 1, 0 })
+    vim.fn.setpos("'m", { 0, vim.fn.search('^  methods', 'n'), 1, 0 })
+    vim.fn.setpos("'p", { 0, vim.fn.search('^  props', 'n'), 1, 0 })
+    vim.fn.setpos("'v", { 0, vim.fn.search('^  validations', 'n'), 1, 0 })
+    vim.fn.setpos("'w", { 0, vim.fn.search('^  watch', 'n'), 1, 0 })
   end
 })
